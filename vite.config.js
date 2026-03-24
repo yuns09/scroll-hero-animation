56d63df19aac3530fb33537686b5b258747699b1
Mac <mac@Yunuu-Macs.local>
2026-03-24 23:03:03 +0530

    Fix Vercel base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/scroll-hero-animation/'
-})
+  plugins: [react(), tailwindcss()],
+  base: "/",
+});
